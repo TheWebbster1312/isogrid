2f5aff2cb0e4c014e6a27c8ea7f58d50613a64b6
SSO\a1076971 <swebb2@sensata.com>
2022-07-27 08:36:02 +0100

    added mous ijk

--- builder.js
+++ builder.js
@@ -2,15 +2,18 @@ var mouse = {
 	x:undefined,
 	y:undefined,
 	i: undefined,
-	j: undefined
+	j: undefined,
+	k:undefined
 }
 //=======================================
 window.addEventListener('mousemove',
 	function(event){
 		mouse.x = event.x;
 		mouse.y = event.y;
-		let mouseMatrix = toIJ(mouse.x, mouse.y, true);
+		let mouseMatrix = toIJ(mouse.x-16, mouse.y-8, true);
 		let [i, j] = mouseMatrix.getCol(0);
+		mouse.i = i
+		mouse.j = j
 		mouseTile.updateCoords(i, j)
 ;})
 
