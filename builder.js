74d5cb065151eb4b4e2e03e03e3b76549d3ff489
SSO\a1076971 <swebb2@sensata.com>
2022-07-27 09:09:29 +0100

    post merge

--- builder.js
+++ builder.js
@@ -1,33 +1,19 @@
 var mouse = {
-	x:undefined,
-	y:undefined,
-<<<<<<< HEAD
+	x: undefined,
+	y: undefined,
 	i: undefined,
 	j: undefined,
-	k:undefined
-=======
-	i:undefined,
-	j:undefined
->>>>>>> origin/master
+	k: undefined
 }
 //=======================================
 window.addEventListener('mousemove',
 	function(event){
-<<<<<<< HEAD
 		mouse.x = event.x;
 		mouse.y = event.y;
 		let mouseMatrix = toIJ(mouse.x-16, mouse.y-8, true);
 		let [i, j] = mouseMatrix.getCol(0);
 		mouse.i = i
 		mouse.j = j
-=======
-		mouse.x = event.x - 16;
-		mouse.y = event.y - 8;
-		let mouseMatrix = toIJ(mouse.x, mouse.y, true);
-		let [i, j] = mouseMatrix.getCol(0);
-		mouse.i = i;
-		mouse.j = j;
->>>>>>> origin/master
 		mouseTile.updateCoords(i, j)
 ;})
 
